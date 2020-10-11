b01eb19fe01c02001333b419bf0973cb4be5c12a
NITISH KUMAR SONTHALIA <53121643+nks102000@users.noreply.github.com>
2020-10-11 23:31:49 +0530

    6.Common-Elements-2-sorted-list-rev-order

---
+++ Programs/6.Common-Elements-2-sorted-list-rev-order.c
@@ -0,0 +1,40 @@
+//Given 2 sorted list and return a list with common elements from both the list(Rev order as entered 
+//as in stack).
+#include<stdio.h>
+#include<stdio.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void creat_list(struct node **st,int n){
+    struct node *new1 = (struct node *)malloc(sizeof(struct node));
+    new1->data=n;
+    new1->next=*st;
+    *st=new1;
+}
+
+void Common_Elements_2_sorted_list_rev_order(struct node **st1,struct node **st2,struct node **head){
+    struct node *t1,*t2;
+    t1=*st1;
+    t2=*st2;
+    while(t1!=NULL&&t2!=NULL){
+        if(t1->data==t2->data){
+            creat_list(head,t1->data);
+            t1=t1->next;
+            t2=t2->next;
+            //Common_Elements_2_sorted_list_rev_order(t1,t2,head);
+        }
+        else if(t1->data>t2->data){
+            t1=t1->next;
+            //Common_Elements_2_sorted_list_rev_order(t1,t2,head);
+        }
+        else{
+            t2=t2->next;
+            //Common_Elements_2_sorted_list_rev_order(t1,t2,head);
+        }
+    }
+    
+}
