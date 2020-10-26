c52a88f07e6e6215fb21c38c1322549604017271
NITISH KUMAR SONTHALIA <53121643+nks102000@users.noreply.github.com>
2020-10-26 22:10:34 +0530

    27.We have pointer at kth element and we have to del that node.(Dont have head pointer)

---
+++ Programs/27.Del-node-with-pointer.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void creat_list(struct node **st ,int n){
+
+    struct node *temp;
+
+    struct node *new1 = (struct node *)malloc(sizeof(struct node));
+
+    new1->data=n;
+
+    new1->next=NULL;
+
+    if(*st==NULL){
+        *st=new1;
+    }
+    else{
+        temp=*st;
+        while(temp->next!=NULL){
+            temp= temp->next;
+        }
+        temp->next = new1;
+    }
+}
+
+void del_node_with_pointer_on(struct node *st){
+    struct node *t=st->next;
+    st->data=st->next->data;
+    st->next=st->next->next;
+    free(t);
+}
